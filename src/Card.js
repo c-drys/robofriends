2d5d91dc418e45b3840096f316142020b53b336d
drystar <chris.drysdale12@gmail.com>
2020-02-13 12:35:49 -0800

    clean up: destructuring

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Card = props => {
+  const { name, emai, idl } = props;
   return (
-    <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       {/* // styling using tachyons  */}
-      <img alt="robots" src="https://robohash.org/chrised?200x200" />
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      {/* template strings used to randomise robot id image  */}
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
