39d0ba76d28e9136f916664eb063557fa32dda04
drystar <chris.drysdale12@gmail.com>
2020-02-13 12:28:24 -0800

    fix typos

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
 export const robots = [
   {
     id: 1,
-    name: 'Chris Drys',
-    username: 'chrise.d',
-    email: 'chrise.d@gmmal.ca'
+    name: "Chris Drys",
+    username: "chrise.d",
+    email: "chrise.d@gmmal.ca"
   },
   {
     id: 2,
-    name: 'Zach Scull',
-    username: 'scullerator',
-    email: 'thonscull@hey.tv'
+    name: "Zach Scull",
+    username: "scullerator",
+    email: "thonscull@hey.tv"
   },
   {
     id: 3,
-    name: 'Hilda the K',
-    username: 'Khajehamiri',
-    email: 'hk162@serverless.ca'
+    name: "Hilda the K",
+    username: "Khajehamiri",
+    email: "hk162@serverless.ca"
   },
   {
     id: 4,
-    name: 'Nick Hosack',
-    username: 'itsNicky',
-    email: 'thehos@itsnicky.org'
+    name: "Nick Hosack",
+    username: "itsNicky",
+    email: "thehos@itsnicky.org"
   },
   {
     id: 5,
-    name: 'Taylor W',
-    username: 'westeros',
-    email: 'westisbest@tw.ca'
+    name: "Taylor W",
+    username: "westeros",
+    email: "westisbest@tw.ca"
   },
   {
     id: 6,
-    name: 'Mr Allan',
-    username: 'coffeelover',
-    email: 'alpack@theonly.com'
+    name: "Mr Allan",
+    username: "coffeelover",
+    email: "alpack@theonly.com"
   },
   {
     id: 7,
-    name: 'liv ',
-    username: 'liv.a.little',
-    email: 'liv123@outthere.com'
+    name: "liv ",
+    username: "liv.a.little",
+    email: "liv123@outthere.com"
   },
   {
     id: 8,
-    name: 'Rebecca Reyes',
-    username: 'ReyesUp',
-    email: 'reyes.r@outlook.com.au
+    name: "Rebecca Reyes",
+    username: "ReyesUp",
+    email: "reyes.r@outlook.com.au"
   },
   {
     id: 9,
-    name: 'Anna Fur',
-    username: 'Anna4',
-    email: 'anna@macncheese.io'
+    name: "Anna Fur",
+    username: "Anna4",
+    email: "anna@macncheese.io"
   },
   {
     id: 10,
-    name: 'Marc Logs',
-    username: 'loggit',
-    email: 'marc.love@nopets.biz'
+    name: "Marc Logs",
+    username: "loggit",
+    email: "marc.love@nopets.biz"
   }
 ];
