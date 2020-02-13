bcd500b697c32ce88fa8a818ffa21b9dc2a9694e
drystar <chris.drysdale12@gmail.com>
2020-02-13 12:37:58 -0800

    destructure within the parameters | even cleaner

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const Card = props => {
-  const { name, emai, idl } = props;
+const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       {/* // styling using tachyons  */}
