ca3a19d8ee702ece15bebdbdac5954df41077e72
drystar <chris.drysdale12@gmail.com>
2020-02-13 12:29:23 -0800

    create dynamic names | passing as props

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Card = () => {
+const Card = props => {
   return (
     <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       {/* // styling using tachyons  */}
       <img alt="robots" src="https://robohash.org/chrised?200x200" />
       <div>
-        <h2>Chrise D</h2>
-        <p>chrise.d@gmail.ca</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
