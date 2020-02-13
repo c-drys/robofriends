1a33426f4dc29b48500459b5a85e5fb3050c1672
drystar <chris.drysdale12@gmail.com>
2020-02-13 02:44:15 -0800

    update with new card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robots" src="https://robohash.org/chrised" />
+      <div>
+        <h2>Chrise D</h2>
+        <p>chrise.d@gmail.ca</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
