ddafcb704cc5ad3fea28e982cadc86647fa84cdf
drystar <chris.drysdale12@gmail.com>
2020-02-14 00:29:08 -0800

    setState for onSearchChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import Searchbox from "./SearchBox";
+import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
 class App extends Component {
@@ -11,20 +11,24 @@ class App extends Component {
       searchfield: ""
     };
   }
-  onSearchChange(event) {
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
     console.log(filteredRobots);
-  }
-  render() {
+
     return (
       <div className="tc">
         <h1> RoboFriends</h1>
-        <Searchbox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
