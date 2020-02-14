44298c914a23e196123964c6c9e41ac7f1655d57
drystar <chris.drysdale12@gmail.com>
2020-02-13 23:57:39 -0800

    create class based component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import Searchbox from "./SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1> RoboFriends</h1>
-      <Searchbox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    };
+  }
+  onSearchChange(event) {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobots);
+  }
+  render() {
+    return (
+      <div className="tc">
+        <h1> RoboFriends</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
