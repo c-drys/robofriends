05a77d82cdf98be2b13f1deaccdf39e22ac6a483
drystar <chris.drysdale12@gmail.com>
2020-04-03 22:39:36 -0700

    create search reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,3 +3,13 @@ import { CHANGE_SEARCH_FIELD } from "./constants";
 const initialState = {
   searchField: ""
 };
+
+export const searchRobots = (state = initialStateSearch, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCHFIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    // return { ...state, searchField: action.paload}
+    default:
+      return state;
+  }
+};
