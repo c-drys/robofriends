b3ee8103210fb58065ea58a93340c07b6759ece3
drystar <chris.drysdale12@gmail.com>
2020-04-05 22:29:38 -0700

    fetch no longer required | replaced

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -33,11 +33,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => {
-        this.setState({ robots: users });
-      });
+    this.props.onRequestRobots;
   }
 
   render() {
